81d84e93f2a0b2ed3019bb07b98318863206ce7b
Barend Erasmus <developersworkspace@gmail.com>
2017-10-19 14:35:33 +0200

    Adds setting controller and views

---
+++ SourceCode/EPONS/EPONS.Teddy.Presentation/Controllers/SettingController.cs
@@ -0,0 +1,24 @@
+﻿using EPONS.Teddy.Application.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPONS.Teddy.Presentation.Controllers
+{
+    public class SettingController : BaseController
+    {
+        [HttpGet]
+        [Authorize(Order = 4)]
+        public ActionResult Index()
+        {
+            var baseObject = GetBaseObject();
+
+            if (!baseObject.User.IsSuperAdmin)
+                new BusinessRuleException("Access Denied");
+
+            return View();
+        }
+    }
+}
