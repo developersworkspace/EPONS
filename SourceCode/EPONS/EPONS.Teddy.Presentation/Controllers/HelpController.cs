07bb745bcf5010db72c63b667c9a2558c18b31b2
Barend Erasmus <developersworkspace@gmail.com>
2017-11-21 20:38:35 +0200

    update

---
+++ SourceCode/EPONS/EPONS.Teddy.Presentation/Controllers/HelpController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPONS.Teddy.Presentation.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult UserManual()
+        {
+            return new FileContentResult(System.IO.File.ReadAllBytes(ConfigurationManager.AppSettings["UserManualPath"]), "application/pdf");
+        }
+    }
+}
