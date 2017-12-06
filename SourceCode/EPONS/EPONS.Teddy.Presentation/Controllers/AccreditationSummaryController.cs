15d242dda4876d0a1a0f18eac280a7d48f49f960
Barend Erasmus <developersworkspace@gmail.com>
2017-12-06 13:58:20 +0200

    adds ui for summary

---
+++ SourceCode/EPONS/EPONS.Teddy.Presentation/Controllers/AccreditationSummaryController.cs
@@ -0,0 +1,21 @@
+﻿using EPONS.Teddy.Application.Exceptions;
+using System.Web.Mvc;
+
+namespace EPONS.Teddy.Presentation.Controllers
+{
+    public class AccreditationSummaryController : BaseController
+    {
+
+        [HttpGet]
+        [Authorize(Order = 4)]
+        public ActionResult Index()
+        {
+            var baseObject = GetBaseObject();
+
+            if (!baseObject.User.IsSuperAdmin)
+                new BusinessRuleException("Access Denied");
+
+            return View(baseObject);
+        }
+    }
+}
