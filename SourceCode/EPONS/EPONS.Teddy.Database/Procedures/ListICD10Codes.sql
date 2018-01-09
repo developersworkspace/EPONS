11299fb13b15d0b5fa12787b009b7dfbd1daa5ea
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-01-09 15:32:38 +0200

    update

--- SourceCode/EPONS/EPONS.Teddy.Database/Procedures/ListICD10Codes.sql
+++ SourceCode/EPONS/EPONS.Teddy.Database/Procedures/ListICD10Codes.sql
@@ -9,7 +9,7 @@ SELECT TOP 20
 [icd10Code].[ICD10CodeId] AS [Id],
 [icd10Code].[Code] + ' - ' + [icd10Code].[Name] AS [Name]
 FROM [ValueObjects].[ICD10Codes] AS [icd10Code]
-WHERE [icd10Code].[Code] + ' - ' + [icd10Code].[Name] LIKE '%' + @query + '%'
+WHERE [icd10Code].[Code] + ' - ' + [icd10Code].[Name] = @query
 ORDER BY [icd10Code].[Code] + ' - ' + [icd10Code].[Name] ASC
 
 EXEC [Audit].[CreateLogEntry] '[EPONS].[ListICD10Codes]', 'Executed'
